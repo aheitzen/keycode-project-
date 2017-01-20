db660d7620e4e7ceea290b194f29900d90c09f2a
aheitzen <alexaheitzenrader@yahoo.com>
2017-01-20 15:34:30 -0800

    jQuery gradient stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+var gradient = ['linear-gradient(#F84592, #9429E4)', 'linear-gradient(#14D2BC, #1783d6)', 'linear-gradient(#FE7105, #FE22C9)'];
+var toggle = 1;
 $(document).ready(function () {
 	$(document).keypress(function(e) {
 		$('body').text(e.which);
+		$('body').css('background', gradient[toggle])
+		if (toggle < gradient.length -1) {
+			toggle++;
+		} else {
+			toggle = 0;
+		}
 	});
 });
+
 
 //window, document, html
