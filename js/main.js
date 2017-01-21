93e87c34f47557b16ae16360f07247f45b4df1e3
aheitzen <alexaheitzenrader@yahoo.com>
2017-01-20 16:59:04 -0800

    simple functionlaity

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var gradient = ['linear-gradient(#F84592, #9429E4)', 'linear-gradient(#14D2BC, #1783d6)', 'linear-gradient(#FE7105, #FE22C9)'];
+var gradient = ['linear-gradient(#F84592, #9429E4)', 'linear-gradient(#14D2BC, #1783d6)', 'linear-gradient(#FE7105, #FE22C9)', 'linear-gradient(#9C4EDF, #505BC8)', 'linear-gradient(#1CAEAD, #2AD475)'];
 var toggle = 1;
 $(document).ready(function () {
 	$(document).keypress(function(e) {
+		console.log(e);
 		$('#numberHolderDiv').text(e.which);
 		$('#numberHolderDiv').css('background', gradient[toggle])
 		if (toggle < gradient.length -1) {
@@ -14,3 +15,5 @@ $(document).ready(function () {
 
 
 //window, document, html
+
+
