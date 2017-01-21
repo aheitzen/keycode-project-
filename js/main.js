d8f33bec2e9727351165149695d0544260d764ad
aheitzen <alexaheitzenrader@yahoo.com>
2017-01-20 16:49:16 -0800

    style stuffs

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var gradient = ['linear-gradient(#F84592, #9429E4)', 'linear-gradient(#14D2BC, #
 var toggle = 1;
 $(document).ready(function () {
 	$(document).keypress(function(e) {
-		$('body').text(e.which);
-		$('body').css('background', gradient[toggle])
+		$('#numberHolderDiv').text(e.which);
+		$('#numberHolderDiv').css('background', gradient[toggle])
 		if (toggle < gradient.length -1) {
 			toggle++;
 		} else {
